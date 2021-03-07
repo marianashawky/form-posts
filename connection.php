2443a1b6981b6d3543d307982580fb6f31a283e2
marianashawky <76921092+marianashawky@users.noreply.github.com>
2021-03-07 21:28:11 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,45 @@
+<?php 
+
+$server   = "localhost";
+$userName = "root";
+$password = "";
+$dbName   = "posts";
+
+    $con = mysqli_connect($server,$userName,$password,$dbName);
+
+     if(!$con){
+         die('error : '.mysqli_connect_error());
+     }else{
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $title = strip_tags(htmlspecialchars(trim($_POST['title'])));  
+        $content = $_POST['content'];
+        $created= date("d/m/Y  h:i:sa ");
+   
+
+        $sql  = "insert into posts (title,content,created) values ('$title','$content','$created') "; 
+
+        $result =  mysqli_query($con,$sql);
+
+        if($result){
+            echo 'success';
+        }
+
+
+
+
+
+        }
+
+
+
+
+
+
+     }
+
+
+
+
+?>
